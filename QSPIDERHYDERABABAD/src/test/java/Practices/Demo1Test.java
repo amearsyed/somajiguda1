3e09b585643045f2d6bf9d778c6ef67af0b6c38a
Syed Amear <Syed Amear@LAPTOP-VHSAH8S1>
2022-09-13 14:46:57 +0530

    Added demo1&3

---
+++ QSPIDERHYDERABABAD/src/test/java/Practices/Demo1Test.java
@@ -0,0 +1,9 @@
+package Practices;
+
+public class Demo1Test {
+	public static void main(String[] args) {
+		String str="Hello world";
+		System.out.println(str);
+	}
+
+}
---
+++ QSPIDERHYDERABABAD/src/test/java/Practices/Demo3Test.java
@@ -0,0 +1,9 @@
+package Practices;
+
+public class Demo3Test {
+	public static void main(String[] args) {
+		String s="hi";
+		System.out.println(s);
+	}
+
+}
